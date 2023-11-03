8f2e9f7ee2b51bf9d72161fb8cb9797a7452e645
HCVincent <lilacsweet@163.com>
2023-11-03 14:51:04 +0800

    course update

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,6 +3,8 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
+import TitleForm from "./_components/title-form";
+import DescriptionForm from "./_components/description-form";
 
 const CrouseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -45,6 +47,8 @@ const CrouseIdPage = async ({ params }: { params: { courseId: string } }) => {
             <IconBadge size="sm" variant="success" icon={LayoutDashboard} />
             <h2 className="text-xl">Customize your course</h2>
           </div>
+          <TitleForm initialData={course} courseId={course.id} />
+          <DescriptionForm initialData={course} courseId={course.id} />
         </div>
       </div>
     </div>
