eb35ec0cafb211cfa99970f9118d9129c0371fa6
HCVincent <lilacsweet@163.com>
2023-11-11 12:06:51 +0800

    logo

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -6,14 +6,13 @@ import { useRouter } from "next/navigation";
 const Logo = () => {
   const router = useRouter();
   return (
-    <Image
-      height={100}
-      width={100}
-      alt="logo"
-      src="/logo.svg"
-      className="cursor-pointer"
+    <div
+      className="flex flex-col cursor-pointer"
       onClick={() => router.push("/")}
-    />
+    >
+      <Image height={100} width={100} alt="logo" src="/logo.svg" />
+      <span className="font-bold text-2xl">Courses</span>
+    </div>
   );
 };
 
